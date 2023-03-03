ef1ed7766ecd8fe6379fd7d64498a7c341d5e08b
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-03 08:50:26 -0500

    Add Player methods

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -1,5 +1,108 @@
-export class Player {
-    public score = 0;
+import { TileType, tilePoint } from "../constants";
+import { Board } from "./board";
+import { Tile } from "./tile";
+import { VonNeumannNeighborhoods, getNeighborhoodParams } from './von-neumann-neighborhoods';
 
-    constructor ( public name: string ) { }
+export class Player {
+    public hand: Tile[] = [];
+    public discardsCounter = 5;
+    public score = 0;
+    public turn = 0;
+
+    constructor ( public name: string, private board: Board ) { }
+
+
+    /**
+     * "If the player has not used up all of their discards, then the player's hand is emptied, the
+     * turn is incremented, the discard counter is decremented, and the player draws four new tiles."
+     * 
+     * The first thing we do is check if the player has used up all of their discards. If they have,
+     * then we throw an error. If they haven't, then we empty the player's hand, increment the turn,
+     * decrement the discard counter, and draw four new tiles.
+     * @throws {Error} If there are no more opportunities to discard the hand 
+     */
+    public discardHand (): void {
+        if ( this.discardsCounter <= 0 ) throw new Error( 'You cannot discard your current hand' );
+
+        this.hand = [];
+        this.turn += 1;
+        this.discardsCounter -= 1;
+
+        for ( let i = 0; i < 4; i++ ) {
+            this.appendTileToHand();
+        }
+    }
+
+
+    public updateHand ( tile: Tile ) {
+        this.hand.splice( this.hand.indexOf( tile ), 1 );
+
+        this.turn += 1;
+        this.appendTileToHand();
+    };
+
+
+    /**
+     * Drawing a tile from the deck and adding it to the player's hand. 
+     */
+    public appendTileToHand (): void {
+        this.hand.push( Tile.generateTile() );
+    };
+
+
+    /**
+     * "This function takes a tile type, a neighborhood params, and a number of points, and increases the
+     * score by the number of points for each tile of the given type in the neighborhood."
+     * 
+     * @param tileType - keyof typeof TileType
+     * @param {getNeighborhoodParams} neighborhoodParams - getNeighborhoodParams
+     * @param {number} points - number - the amount of points to add to the score
+     */
+    private _increasePoints ( tileType: keyof typeof TileType, neighborhoodParams: getNeighborhoodParams, points: number ): void {
+        const tilesNeighborhood: number[][] = VonNeumannNeighborhoods.getNeighborhood( neighborhoodParams, tileType === TileType.ABBEY );
+
+        tilesNeighborhood.forEach( ( nt ) => {
+            const tile = this.board.getTile( nt[ 0 ], nt[ 1 ] );
+            if ( tile?.type === tileType || ( tileType === TileType.ABBEY && tile ) )
+                this.score += points;
+        } );
+    };
+
+
+    /**
+     * "The function takes a tile type, row and column as parameters and updates the score based on the
+     * tile type and its neighborhood."
+     * 
+     * The function is called from the following function:
+     * @param tileType - keyof typeof TileType, row: number, column: number
+     * @param {number} points - number - the amount of points to add to the score
+     * @param {number} column - number,
+     */
+    public updateScore ( tileType: keyof typeof TileType, row: number, column: number, size: number ): void {
+        const params: getNeighborhoodParams = {
+            row, column,
+            maxRows: size, maxColumns: size
+        };
+
+        switch ( tileType ) {
+            case TileType.ROAD:
+                this.score += tilePoint.road;
+                this._increasePoints( TileType.ABBEY, params, tilePoint.abbey );
+                break;
+
+            case TileType.CITY:
+                this.score += tilePoint.city;
+                this._increasePoints( TileType.ABBEY, params, tilePoint.abbey );
+                this._increasePoints( TileType.CITY, params, tilePoint.chain );
+                break;
+
+            case TileType.ABBEY:
+                this.score += tilePoint.abbey;
+                this._increasePoints( TileType.ABBEY, params, tilePoint.abbey );
+                break;
+
+            default:
+                break;
+        }
+    };
 }
